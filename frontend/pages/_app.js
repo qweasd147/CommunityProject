8996fc7fb069c4d4531b1b07068ee1445a04f95b
joohyung <qweasd_147@naver.com>
2019-06-14 18:27:58 +0900

    material ui 적용 & 레이아웃 1차 분리

---
+++ frontend/pages/_app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import App, { Container } from 'next/app'
+import Layout from '../common/components/Layout'
+
+export default class MyApp extends App {
+    constructor(props){
+        super(props)
+    }
+
+    static async getInitialProps({ Component, router, ctx }) {
+      
+      let pageProps = {}
+  
+      if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx)
+      }
+      return { pageProps }
+    }
+  
+    render () {
+        const { Component, pageProps } = this.props
+        
+        return (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        );
+    }
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
+
+export default function Dashboard() {
+  const classes = useStyles();
+
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper className={fixedHeightPaper}>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper className={fixedHeightPaper}></Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}></Paper>
+            </Grid>
+          </Grid>
+    </Container>
+  );
+}
